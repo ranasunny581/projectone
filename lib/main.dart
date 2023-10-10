533e43f738127c98c1e46c09bd0cf4e9e42cc446
param <streesafetyapp@gmail.com>
2023-10-10 21:55:39 +0530

    my files

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Charan Singh'),
+      home: const MyHomePage(title: 'Charan Singh demo'),
     );
   }
 }
