61ab09ac5f7266dd7ab2c776e622b837f0e2dc5f
param <streesafetyapp@gmail.com>
2023-10-11 22:27:23 +0530

    class  variables

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,75 @@ main(){
   this is my new code
   i am going now
   multiline comments
-   */
+
 
   //variables types
-  final int age; //-5,-10,10,5,7,8
-  const pie=3.14;
-  String name='Charan Singh';
-  double marks=90.6;// 2.08,2.32589
-  bool edu=true;//true,false
-  age=30;
+  // final int age; //-5,-10,10,5,7,8
+  // const pie=3.14;
+  // String name='Charan Singh';
+  // double marks=90.6;// 2.08,2.32589
+  // bool edu=true;//true,false
+  // age=30;
+  //
+  // name="hello";
+  // var person=false;
+  // person=true;
+  // dynamic val=5;
+  // val=70;
+  //
+  // print(person.runtimeType);
 
-  name="hello";
-  var person=false;
-  person=true;
-  dynamic val=5;
-  val=70;
+  // print("My name is $name and my age is  $age");
+  // print("My tenth marks % is $marks");
+  // print(" are u educated ? $edu");
+  */
 
-  print(person.runtimeType);
+  /*
+  //null variable/values
 
-  print("My name is $name and my age is  $age");
-  print("My tenth marks % is $marks");
-  print(" are u educated ? $edu");
+  // int? x;
+    // x=5;
+  // print(x??"no value");
+  // print(x??=9);
+  // print(x?.isEven);
+  // print(x!);
+
+  */
+  /*
+  //operators + - * / %
+  // double x=10.8;
+  // double  y=2.5;
+  // double res=x/y;
+  // // int res=x~/y;
+  // var res=84.0+2;
+  // var res=(43!=42);
+
+  // var res=10.0;
+  // res=res+1;
+  // res++; //post increment
+  // ++res;  //pre increment
+  //  print(++res);
+  // res=res+2;
+  // res+=-1.5;
+  // int x=20,y=20;
+// relational operator
+//  > <  >=  <=   ==   !=
+
+  //logical opertators
+  //  && ||
+  // (game=="cricket") || (game>'football')
+  // ((gender=="male")  && (age>=21))  || ((gender ==""female) && (age>=18))
+  */
+
+  //strings
+  String fname='Charan';
+  String lname="Singh";
+  //concatenation
+  String fullname=fname+' '+lname;
+  //String interpolation
+  // print(fname.length);
+  print("My first name length is ${fname.length}         and last name is      $lname");
+// escape sequence  // raw printing
+  print(r"sorry i \ can't come");
+
 }
