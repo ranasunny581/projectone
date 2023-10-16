7e8942e873a4f48b747493a28a615cb1b9b45510
param <streesafetyapp@gmail.com>
2023-10-16 22:44:24 +0530

    class lists

--- lib/main.dart
+++ lib/main.dart
@@ -206,18 +206,109 @@ main(){
       //   1 2 1
       // 1 2 3 2 1
       //
-   int v=0;
-   for(int x=1;x<=5;x++){
-     //printing space
-     for(int z=1;z<=5-x;z++){
-       stdout.write(" ");
-     }
-     //printing number
-     for(int y=1;y<=x;y++){
-        stdout.write("1 ");
-     }
-     stdout.writeln();
-   }
+   // int v=0;
+   // for(int x=1;x<=5;x++){
+   //   //printing space
+   //   for(int z=1;z<=5-x;z++){
+   //     stdout.write(" ");
+   //   }
+   //   //printing number
+   //   for(int y=1;y<=x;y++){
+   //      stdout.write("1 ");
+   //   }
+   //   stdout.writeln();
+   // }
+
+  // int a=40;  //ascii codes  A=65 Z=90  a=97 z=122
+  // for(int x=a;x<=122;x++)
+  //   stdout.write(String.fromCharCode(x));
+ // int v=65;
+ //  for(int r=1;r<=4;r++){
+ //    for(int c=1;c<=r;c++){
+ //      stdout.write(String.fromCharCode(v++)+" ");
+ //    }
+ //    stdout.writeln();
+ //  }
+
+  //lists
+  // List<String> mylist=["charan","preet","sahil","ronak"];
+  // List<int> nums=[1,2,3,4,5,6];
+  // List custom=[1,2,"helo","rahul",10.9,true];
+  // print(custom);
+  // print(nums);
+  // print(mylist);
+
+ //  List<String> l=[];
+ // l.add("Hello");
+ // l.add("Sachin");
+ // l.add("tendulkar");
+ //  print(l);
+ //  l.insert(3, "finish");
+ //  l.insert(0, "start");
+ //  print(l);
+ //  l.remove("Hello");
+ //  print(l);
+ //
+ //  for(int x=0;x<l.length;x++){
+ //    print(l.elementAt(x));
+ //  }
+ // print("-----------------------");
+ //
+ //  for(String x in l){
+ //    print(x);
+ //  }
+ //  print("-----------------------");
+ //
+ //  l.forEach((element) {
+ //    print(element);
+ //  });
+ //
+ //  List l1=[1,2,3,4,5];
+ //  List l2=[4,5,6,7,8];
+ //  List l3=l1+l2;
+ //  print(l3);
+ //
+ //  //set  {1,2,3}
+ //  var s=<dynamic>{};
+ //  s.add(1);
+ //  s.add(2.01);
+ //  s.add(2.00);
+ //  print(s.contains(2.02));
+ //  print(s);
+ //
+ //  Set s1={1,2,3,4,5};
+ //  Set s2={4,5,6,7,8};
+ //  Set s3={};
+ //  s3.addAll(l3);
+ //  print(s3.length);
+
+  //functions
+
+  // sabjilao(int r){
+  //   return r*r*1.0;
+  // }
+  //
+  // double area;
+  // double sqr=sabjilao(6);
+  // area=3.14*sqr;  //pie*r*r
+  // print(area);
+
+  printtable(int v){
+    for(int i=1;i<=10;i++){
+      print("$v x $i = ${v*i}");
+    }
+  }
+
+  // area(int l,int w){
+  //   return l*w;
+  // }
+
+  area(int l,int w)=>l*w;
+
+
+  print(area(6, 11));
+
+
 
 
 }
